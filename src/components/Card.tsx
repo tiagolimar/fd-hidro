ab2bc10c63301bf98a1368ef0c47ea61a99039df
tiagolimar <tiagolima.r2@gmail.com>
2025-06-10 01:23:00 -0300

    feat: creating a component add

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 type CardProps = {
+    className?: string;
     children: React.ReactNode,
     "data-id"?: number
 }
-
 function Card(props : CardProps) {
-    const {children, "data-id": dataId} = props;
+    const {children, "data-id": dataId, className} = props;
     return (
-        <div data-id={dataId} className="border shadow-sm hover:shadow-lg hover:shadow-stone-300 transition-shadow p-4 rounded-lg border-stone-300 flex flex-col gap-2 flex-1 min-w-[200px] text-center">
+        <div data-id={dataId} className={`border shadow-sm hover:shadow-lg hover:shadow-stone-300 transition-shadow p-4 rounded-lg border-stone-300 flex flex-col gap-2 flex-1 min-w-[200px] text-center ${className}`}>
             {children}
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Card from './../components/Card';
+import CardContainer from '../components/CardContainer';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <CardContainer>
+      <Card className="flex justify-center items-center">
+        <img width="50" height="50" src="https://img.icons8.com/ios/50/add--v1.png" alt="add--v1"/>
+      </Card>
+
+    </CardContainer>
   )
 }
 
