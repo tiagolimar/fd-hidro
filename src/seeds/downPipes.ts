69aae1eeeed17c5bf4ee01161d76906bd3bf35c9
Tiago Lima <47084105+tiagolimar@users.noreply.github.com>
2025-08-08 20:51:20 -0300

    Refactor contributions to use IDs and add hydration

--- src/seeds/downPipes.ts
+++ src/seeds/downPipes.ts
@@ -1,21 +1,79 @@
 import type { AppDB } from '@/db/db';
 import { DownPipe } from '@/models/DownPipe';
+import { hydrateContribution } from '@/utils/hydrateContribution';
 
 export async function seedDownPipes(db: AppDB) {
   const systems = await db.systems.toArray();
   const contributions = await db.contributions.toArray();
   const s = (id: number) => systems.find(sys => sys.id === id)!;
-  const c = (id: number) => contributions.find(con => con.id === id)!;
+  const c = async (id: number) =>
+    hydrateContribution(contributions.find(con => con.id === id)!);
 
   const downpipes: DownPipe[] = [
-    new DownPipe(1, '1', 100, s(1), [c(1), c(3), c(5), c(7), c(9), c(11)]),
-    new DownPipe(2, '2', 100, s(1), [c(2), c(4), c(6), c(8), c(10), c(12)]),
-    new DownPipe(3, '1', 100, s(2), [c(1), c(4), c(5), c(8), c(9), c(11)]),
-    new DownPipe(4, '2', 100, s(2), [c(2), c(3), c(6), c(7), c(10), c(12)]),
-    new DownPipe(5, '1', 100, s(3), [c(1), c(4), c(6), c(7), c(9), c(11)]),
-    new DownPipe(6, '2', 100, s(3), [c(2), c(3), c(5), c(8), c(10), c(12)]),
-    new DownPipe(7, '1', 100, s(4), [c(1), c(3), c(5), c(7), c(10), c(11)]),
-    new DownPipe(8, '2', 100, s(4), [c(2), c(4), c(6), c(8), c(9), c(12)]),
+    new DownPipe(1, '1', 100, s(1), [
+      await c(1),
+      await c(3),
+      await c(5),
+      await c(7),
+      await c(9),
+      await c(11),
+    ]),
+    new DownPipe(2, '2', 100, s(1), [
+      await c(2),
+      await c(4),
+      await c(6),
+      await c(8),
+      await c(10),
+      await c(12),
+    ]),
+    new DownPipe(3, '1', 100, s(2), [
+      await c(1),
+      await c(4),
+      await c(5),
+      await c(8),
+      await c(9),
+      await c(11),
+    ]),
+    new DownPipe(4, '2', 100, s(2), [
+      await c(2),
+      await c(3),
+      await c(6),
+      await c(7),
+      await c(10),
+      await c(12),
+    ]),
+    new DownPipe(5, '1', 100, s(3), [
+      await c(1),
+      await c(4),
+      await c(6),
+      await c(7),
+      await c(9),
+      await c(11),
+    ]),
+    new DownPipe(6, '2', 100, s(3), [
+      await c(2),
+      await c(3),
+      await c(5),
+      await c(8),
+      await c(10),
+      await c(12),
+    ]),
+    new DownPipe(7, '1', 100, s(4), [
+      await c(1),
+      await c(3),
+      await c(5),
+      await c(7),
+      await c(10),
+      await c(11),
+    ]),
+    new DownPipe(8, '2', 100, s(4), [
+      await c(2),
+      await c(4),
+      await c(6),
+      await c(8),
+      await c(9),
+      await c(12),
+    ]),
   ];
 
   await db.downpipes.bulkAdd(downpipes);
