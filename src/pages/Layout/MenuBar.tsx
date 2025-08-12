f191dcba50adb8d3662d03de2e2dabe145665fc2
tiagolimar <tiagolima.r2@gmail.com>
2025-08-11 21:30:38 -0300

    change icons in menubar

--- src/pages/Layout/MenuBar.tsx
+++ src/pages/Layout/MenuBar.tsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom'
 
-function ItemMenu({link, icon}: {link: string, icon: string}) {
+function ItemMenu({src, link, icon}: {src: string, link: string, icon: string}) {
     return (
         <Link to={link}>
             <ul className="flex gap-2 items-center ps-4 hover:bg-stone-200 py-2">
-                <img src="https://img.icons8.com/?size=100&id=4819&format=png&color=000000" alt="" width={24} />
+                <img src={src} alt="" width={24} />
                 <span className="text-bold">{icon}</span>
             </ul>
         </Link>
@@ -16,11 +16,14 @@ function MenuBar() {
         <aside className="max-w-[300px] min-w-[200px] min-h-screen bg-blue-100">
             <nav>
                 <li className="list-none flex flex-col text-center justify-evenly bg-stone-300 text-bold">
-                    <ItemMenu link="" icon="Home" />
-                    <ItemMenu link="equipamentos" icon="Peças Hidráulicas" />
-                    <ItemMenu link="conjuntos" icon="Conjuntos de Peças" />
-                    <ItemMenu link="pavimentos" icon="Pavimentos" />
-                    <ItemMenu link="prumadas" icon="Prumadas" />
+                    <ItemMenu src="https://img.icons8.com/windows/32/home.png" link="" icon="Home" />
+                    <hr />
+                    <ItemMenu src="https://img.icons8.com/ios/50/toilet-bowl.png" link="equipamentos" icon="Peças Hidráulicas" />
+                    <ItemMenu src="https://img.icons8.com/external-others-inmotus-design/67/external-Floor-contour-others-inmotus-design.png" link="conjuntos" icon="Conjuntos de Peças" />
+                    <ItemMenu src="https://img.icons8.com/ios/50/building.png" link="pavimentos" icon="Pavimentos" />
+                    <ItemMenu src="https://img.icons8.com/ios/50/water-pipe.png" link="prumadas" icon="Prumadas" />
+                    <hr />
+                    <ItemMenu src="https://img.icons8.com/ios/50/google-sheets.png" link="memorial" icon="Memorial" />
                 </li>
             </nav>
         </aside>
