2b4ae323ff5c79783f52b36ed7fe72d4a76f50db
Tiago Lima <47084105+tiagolimar@users.noreply.github.com>
2025-08-14 20:43:34 -0300

    Hydrate EquipamentSets items on load

--- src/repositories/EquipamentSetRepository.ts
+++ src/repositories/EquipamentSetRepository.ts
@@ -1,16 +1,46 @@
 import { db } from '@/db/db'
-import type { EquipamentSet } from '@/models/EquipamentSet'
+import { EquipamentSet, HydratedEquipamentSet } from '@/models/EquipamentSet'
 import { BaseRepository } from './BaseRepository'
 import {
     toEquipamentSet,
     fromEquipamentSet,
     type EquipamentSetDTO,
 } from '@/dto/equipamentSet'
+import { hydrateEquipamentSet } from '@/utils/hydrateEquipamentSet'
 
-class EquipamentSetRepository extends BaseRepository<EquipamentSet, EquipamentSetDTO> {
+class EquipamentSetRepository {
+    private base: BaseRepository<EquipamentSet, EquipamentSetDTO>
+
     constructor() {
-        super(db.equipamentSets, toEquipamentSet, fromEquipamentSet)
+        this.base = new BaseRepository(
+            db.equipamentSets,
+            toEquipamentSet,
+            fromEquipamentSet,
+        )
+    }
+
+    async getAll(): Promise<HydratedEquipamentSet[]> {
+        const sets = await this.base.getAll()
+        return Promise.all(sets.map(hydrateEquipamentSet))
+    }
+
+    async getById(id: number): Promise<HydratedEquipamentSet | undefined> {
+        const set = await this.base.getById(id)
+        return set ? await hydrateEquipamentSet(set) : undefined
+    }
+
+    create(data: EquipamentSet) {
+        return this.base.create(data)
+    }
+
+    update(id: number, changes: Partial<EquipamentSet>) {
+        return this.base.update(id, changes)
+    }
+
+    delete(id: number) {
+        return this.base.delete(id)
     }
 }
 
 export default new EquipamentSetRepository()
+
--- src/utils/hydrateContribution.ts
+++ src/utils/hydrateContribution.ts
@@ -1,33 +1,30 @@
-import { Contribution, HydratedContribution } from '@/models/Contribution';
-import LevelRepository from '@/repositories/LevelRepository';
-import EquipamentRepository from '@/repositories/EquipamentRepository';
-import EquipamentSetRepository from '@/repositories/EquipamentSetRepository';
-import { hydrateEquipamentSet } from './hydrateEquipamentSet';
+import { Contribution, HydratedContribution } from '@/models/Contribution'
+import LevelRepository from '@/repositories/LevelRepository'
+import EquipamentRepository from '@/repositories/EquipamentRepository'
+import EquipamentSetRepository from '@/repositories/EquipamentSetRepository'
 
 export async function hydrateContribution(
-    contribution: Contribution
+    contribution: Contribution,
 ): Promise<HydratedContribution> {
-    const level = await LevelRepository.getById(contribution.levelId);
+    const level = await LevelRepository.getById(contribution.levelId)
     if (!level) {
-        throw new Error(`Level ${contribution.levelId} not found`);
+        throw new Error(`Level ${contribution.levelId} not found`)
     }
 
     const equipament =
-    contribution.equipamentId !== undefined
-        ? await EquipamentRepository.getById(contribution.equipamentId)
-        : await (async () => {
-            const set = await EquipamentSetRepository.getById(
-                contribution.equipamentSetId!
-            );
-            return set ? await hydrateEquipamentSet(set) : undefined;
-        })();
+        contribution.equipamentId !== undefined
+            ? await EquipamentRepository.getById(contribution.equipamentId)
+            : await EquipamentSetRepository.getById(
+                contribution.equipamentSetId!,
+            )
     if (!equipament) {
         throw new Error(
             `Equipament ${
                 contribution.equipamentId ?? contribution.equipamentSetId
-            } not found`
-        );
+            } not found`,
+        )
     }
 
-    return new HydratedContribution(level, equipament, contribution.id);
+    return new HydratedContribution(level, equipament, contribution.id)
 }
+
